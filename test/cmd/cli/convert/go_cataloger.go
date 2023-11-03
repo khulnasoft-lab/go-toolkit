875cd05c1d1d22a23ac285f5ea075ee87d593483
Md Sulaiman <51925710+sulaiman-coder@users.noreply.github.com>
2023-11-03 08:10:29 +0600

    Create go_cataloger.go
    Signed-off-by: Md Sulaiman <51925710+sulaiman-coder@users.noreply.github.com>

---
+++ test/cmd/cli/convert/go_cataloger.go
@@ -0,0 +1,11 @@
+package convert
+
+import (
+	"github.com/khulnasoft-lab/go-toolkit/test/cmd/app"
+	"github.com/khulnasoft-lab/go-toolkit/test/cmd/cli/options"
+)
+
+func GoCatalogerConfig(c *options.CommandConfig) app.GoCatalogerConfig {
+	cfg := app.GoCatalogerConfig{}
+	return cfg
+}
